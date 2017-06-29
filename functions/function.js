df4216f128726aae10be1343c0ba21c352e9abd8
Rahul <rahulsept2191@gmail.com>
2017-06-29 22:30:46 +0530

    function phase-2

--- functions/function.js
+++ functions/function.js
@@ -143,3 +143,70 @@ var car = deck1.createCardPicker();
 console.info(car);
 var pc = car();
 console.info("card: " + pc.card + " of " + pc.suit);
+//this in callback
+//
+//
+//
+//
+//
+//
+//
+// interface uiElement{
+//     addClickListener(onclick: (this: void, e: Event) => void): void;
+// }
+//
+//
+// class Handler {
+//     info: string;
+//     onClickGood(this: void, e: Event) {
+//         // can't use this here because it's of type void!
+//         console.log('clicked!');
+//     }
+// }
+// let h = new Handler();
+// uiElement.addClickListener(h.onClickGood);
+//
+//
+var Handler = (function () {
+    function Handler() {
+        this.test = function (e) {
+            console.info(e);
+        };
+    }
+    return Handler;
+}());
+var a = new Handler();
+console.info('asd', a.test('asd'));
+var suits = ["hearts", "spades", "clubs", "diamonds"];
+function pickCard(x) {
+    // Check to see if we're working with an object/array
+    // if so, they gave us the deck and we'll pick the card
+    if (typeof x == "object") {
+        var pickedCard_1 = Math.floor(Math.random() * x.length);
+        return pickedCard_1;
+    }
+    else if (typeof x == "number") {
+        var pickedSuit = Math.floor(x / 13);
+        return { suit: suits[pickedSuit], card: x % 13 };
+    }
+}
+var myDeck = [{ suit: "diamonds", card: 2 }, { suit: "spades", card: 10 }, { suit: "hearts", card: 4 }];
+var pickedCard1 = myDeck[pickCard(myDeck)];
+console.info("card: " + pickedCard1.card + " of " + pickedCard1.suit);
+var pickedCard2 = pickCard(15);
+console.info("card: " + pickedCard2.card + " of " + pickedCard2.suit);
+function pickCard3(x) {
+    if (typeof x == "object") {
+        var pickedCard_2 = Math.floor(Math.random() * x.length);
+        return pickedCard_2;
+    }
+    else if (typeof x == "number") {
+        var pickedSuit = Math.floor(x / 13);
+        return { suit: suits[pickedSuit], card: x % 13 };
+    }
+}
+var myDeck1 = [{ suit: "diamonds", card: 2 }, { suit: "spades", card: 10 }, { suit: "hearts", card: 4 }];
+var pickedCard4 = myDeck[pickCard(myDeck1)];
+console.info("card: " + pickedCard4.card + " of " + pickedCard4.suit);
+var pickedCard5 = pickCard(15);
+console.info("card: " + pickedCard5.card + " of " + pickedCard5.suit);
--- functions/function.ts
+++ functions/function.ts
@@ -231,3 +231,93 @@ console.info(car);
 let pc = car();
 
 console.info("card: " + pc.card + " of " + pc.suit);
+
+
+
+//this in callback
+//
+//
+//
+//
+//
+//
+//
+
+// interface uiElement{
+//     addClickListener(onclick: (this: void, e: Event) => void): void;
+// }
+//
+//
+// class Handler {
+//     info: string;
+//     onClickGood(this: void, e: Event) {
+//         // can't use this here because it's of type void!
+//         console.log('clicked!');
+//     }
+// }
+// let h = new Handler();
+// uiElement.addClickListener(h.onClickGood);
+//
+//
+class Handler {
+  info: string;
+  test = (e: string) => {
+    console.info(e);
+  }
+}
+let a = new Handler();
+
+
+console.info('asd', a.test('asd'));
+
+
+
+let suits = ["hearts", "spades", "clubs", "diamonds"];
+
+function pickCard(x): any {
+    // Check to see if we're working with an object/array
+    // if so, they gave us the deck and we'll pick the card
+    if (typeof x == "object") {
+        let pickedCard = Math.floor(Math.random() * x.length);
+        return pickedCard;
+    }
+    // Otherwise just let them pick the card
+    else if (typeof x == "number") {
+        let pickedSuit = Math.floor(x / 13);
+        return { suit: suits[pickedSuit], card: x % 13 };
+    }
+}
+
+let myDeck = [{ suit: "diamonds", card: 2 }, { suit: "spades", card: 10 }, { suit: "hearts", card: 4 }];
+let pickedCard1 = myDeck[pickCard(myDeck)];
+console.info("card: " + pickedCard1.card + " of " + pickedCard1.suit);
+
+let pickedCard2 = pickCard(15);
+console.info("card: " + pickedCard2.card + " of " + pickedCard2.suit);
+
+
+
+
+function pickCard3(x: {suit: string; card: number;}[]): number;
+
+function pickCard3(x: number): {suit: string; card: number;};
+
+function pickCard3(x): any{
+	    if (typeof x == "object") {
+        let pickedCard = Math.floor(Math.random() * x.length);
+        return pickedCard;
+    }
+    // Otherwise just let them pick the card
+    else if (typeof x == "number") {
+        let pickedSuit = Math.floor(x / 13);
+        return { suit: suits[pickedSuit], card: x % 13 };
+    }
+
+}
+
+let myDeck1 = [{ suit: "diamonds", card: 2 }, { suit: "spades", card: 10 }, { suit: "hearts", card: 4 }];
+let pickedCard4 = myDeck[pickCard(myDeck1)];
+console.info("card: " + pickedCard4.card + " of " + pickedCard4.suit);
+
+let pickedCard5 = pickCard(15);
+console.info("card: " + pickedCard5.card + " of " + pickedCard5.suit);
